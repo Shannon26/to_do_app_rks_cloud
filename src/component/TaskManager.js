fee0f7bd331376ecf80cd0c1f3d676864130651b
Shannon Dmello <shannondmello@gmail.com>
2023-04-26 22:16:58 +0530

    Fixed Local Storage

--- src/component/TaskManager.js
+++ src/component/TaskManager.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect, useState } from "react";
+import React, { useReducer, useEffect } from "react";
 import PropTypes from "prop-types";
 import TaskInput from "./TaskInput";
 import TaskList from "./TaskList";
@@ -8,79 +8,71 @@ const initialState = [];
 const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TASK":
+      // update localStorage inside the reducer
+      localStorage.setItem(
+        "tasks",
+        JSON.stringify([...state, action.payload])
+      );
       return [...state, action.payload];
     case "TOGGLE_TASK":
+      // update localStorage inside the reducer
+      localStorage.setItem(
+        "tasks",
+        JSON.stringify(
+          state.map((task) =>
+            task.id === action.payload
+              ? { ...task, completed: !task.completed }
+              : task
+          )
+        )
+      );
       return state.map((task) =>
         task.id === action.payload ? { ...task, completed: !task.completed } : task
       );
     case "DELETE_TASK":
+      // update localStorage inside the reducer
+      localStorage.setItem(
+        "tasks",
+        JSON.stringify(state.filter((task) => task.id !== action.payload))
+      );
       return state.filter((task) => task.id !== action.payload);
     default:
       return state;
   }
 };
 
-const useLocalStorage = (key, initialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      console.log(error);
-      return initialValue;
-    }
-  });
-
-  const setValue = (value) => {
-    try {
-      window.localStorage.setItem(key, JSON.stringify(value));
-      setStoredValue(value);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  return [storedValue, setValue];
-};
-
 const TaskManager = () => {
-  const [tasks, dispatch] = useReducer(reducer, initialState);
+  // get the initial value from localStorage or use initialState
+  const [tasks, dispatch] = useReducer(
+    reducer,
+    JSON.parse(localStorage.getItem("tasks")) || initialState
+  );
 
-  const [storedTasks, setStoredTasks] = useLocalStorage("tasks", []);
-
+  // use an empty array as the second argument to useEffect
   useEffect(() => {
-    setStoredTasks(tasks);
-  }, [tasks]);
+    console.log("Component mounted");
+  }, []);
 
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 100000);
-    dispatch({ type: "ADD_TASK", payload: { id, description: task, completed: false }}, () => {
-      // Use setState callback function to ensure state is updated before setting localStorage
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-    });
+    dispatch({ type: "ADD_TASK", payload: { id, description: task, completed: false } });
   };
 
   const toggleTask = (id) => {
-    dispatch({ type: "TOGGLE_TASK", payload: id }, () => {
-    // Use setState callback function to ensure state is updated before setting localStorage
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  });
+    dispatch({ type: "TOGGLE_TASK", payload: id });
   };
 
   const deleteTask = (id) => {
-    dispatch({ type: "DELETE_TASK", payload: id }, () => {
-      // Use setState callback function to ensure state is updated before setting localStorage
-      localStorage.setItem("tasks", JSON.stringify(tasks));
-    });
+    dispatch({ type: "DELETE_TASK", payload: id });
   };
 
-return ( 
-<div className="task-manager"> 
-<h1>Task Manager</h1> 
-<TaskInput addTask={addTask} /> 
-<TaskList tasks={storedTasks} toggleTask={toggleTask} deleteTask={deleteTask} /> 
-</div> 
-); 
+  return (
+    <div className="task-manager">
+      <h1>Task Manager</h1>
+      <TaskInput addTask={addTask} />
+      <TaskList tasks={tasks} toggleTask={toggleTask} deleteTask={deleteTask} />
+    </div>
+  );
 };
 
 export default TaskManager;
